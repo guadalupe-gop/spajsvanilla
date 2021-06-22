e2a6643f3289c53567dc04a96a85ffe87107e454
@gopdev <markus44p@gmail.com>
2021-06-21 21:08:43 -0500

    configurar css para administrar elementos visuales

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -4,18 +4,21 @@ import getData from "../utils/getData";
 const Character = async () => {
   const id = getHash();
   const character = await getData(id);
+  const { image, name, episode, status, species, origin, location, gender } =
+    character;
+
   const view = `
   <div class="Characters-inner">
       <article class="Characters-card">
-        <img src="${character.image}" alt="${character.name}">
-        <h2>${character.name}</h2>
+        <img src="${image}" alt="${name}">
+        <h2>${name}</h2>
         <article class="Characters-card">
-            <h3 class="">Episodios:<span>${character.episode.length}</span></h3>
-            <h3 class="">Status:<span>${character.status}</span></h3>
-            <h3>Species:<span>${character.species}</span></h3>
-            <h3>Gender:<span>${character.gender}</span></h3>
-            <h3>Origin:<span>${character.origin.name}</span></h3>
-            <h3>Last Location:<span>${character.location.name}</span></h3>
+            <h3 class="">Episodios:<span>${episode.length}</span></h3>
+            <h3 class="">Status:<span>${status}</span></h3>
+            <h3>Species:<span>${species}</span></h3>
+            <h3>Gender:<span>${gender}</span></h3>
+            <h3>Origin:<span>${origin.name}</span></h3>
+            <h3>Last Location:<span>${location.name}</span></h3>
         </article>
       </article>
   </div>  
